ba2287797d8289c7c32b9834850705f451b73795
Lindsey Mysse <lindsey.mysse@gmail.com>
2013-12-26 07:39:32 -0800

    some more string handling shiz

--- pomodoro.rb
+++ pomodoro.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-
 #extend String
 class String
   def is_number?
@@ -11,36 +10,66 @@ class String
   	true if String(self) rescue false
   end
 
+   # colorization
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def pink
+    colorize(35)
+  end
+
 end
-
-
-logo = IO.read("logo.txt")
-
-puts logo
 
 def ding
 	exec("afplay sounds/ding.wav")
 end
 
-length = 25
-task = "Pomodoro"
 
+logo = IO.read("logo.txt")
 
-puts "I was passed:"
+puts logo.red
+
+#      _       __             _ _       
+#   __| | ___ / _| __ _ _   _| | |_ ___ 
+#  / _` |/ _ \ |_ / _` | | | | | __/ __|
+# | (_| |  __/  _| (_| | |_| | | |_\__ \
+#  \__,_|\___|_|  \__,_|\__,_|_|\__|___/
+#                                       
+
+timeInMinutes = 25
+timeInSeconds = 25 * 60
+task = ""
+
 
 ARGV.each do |value|
 	if value.is_number?
 		puts value + " minutes"
-		length = value.to_i * 60
+		timeInMinutes = value
+		timeInSeconds = value.to_i * 60
 	elsif value.is_string? 
-		puts "Task: " + value
+		task << value + " "
 	end
 
 
 end
 
 
-puts length
+puts timeInSeconds
+puts timeInMinutes
+puts task
 ding
 
 
